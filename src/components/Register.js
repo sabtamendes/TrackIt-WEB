8e9e256b4790c7143a9d13db737bb7278b3aa0ef
Sabta Mendes Alencar <sabtaamendes@gmail.com>
2022-10-18 13:21:22 -0400

    layout página cadastro

---
+++ src/components/Register.js
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom"
+export default function Register() {
+    return (
+        <Container>
+            <input placeholder="email"></input>
+            <input placeholder="senha"></input>
+            <input placeholder="nome"></input>
+            <input placeholder="foto"></input>
+            <button>Entrar</button>
+            <StyledLink to={"/"}>Já tem uma conta? Faça login!</StyledLink>
+        </Container>
+    )
+}
+const Container = styled.div`
+    margin-top: 35vh;
+    padding:15%;
+input{
+   margin-left: -5%;
+   margin-bottom: 4%;
+   width:38vh;
+   padding: 15px; 
+   border:1px solid #D4D4D4;
+   border-radius: 5px;
+   &::placeholder{
+    font-family: 'Lexend Deca', sans-serif;
+    font-size:15px;
+    color:#DBDBDB;
+   }
+}
+button{
+    margin-left: -5%;
+    margin-bottom:15%;
+    padding: 15px 118px;
+    font-weight:600;
+    font-size: 15px;
+    color: #FFFFFF;
+    background-color:#52B6FF;
+    border-radius: 5px;
+    border:none;
+}
+`
+const StyledLink = styled(Link)`
+    margin-top:5%;
+    margin-left:8.5%;
+    font-size:14px;
+    color: #52B6FF;
+`
